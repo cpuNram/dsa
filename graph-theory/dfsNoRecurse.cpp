961136bc058c042bda9c5475f243aa587689bcde
cpuNram <rajeshamg@yahoo.com>
2017-10-09 21:00:51 -0500

    Update dfsNoRecurse.cpp

--- graph-theory/dfsNoRecurse.cpp
+++ graph-theory/dfsNoRecurse.cpp
@@ -3,61 +3,47 @@
 #include <stack>
 
 using namespace std;
-
 class Graph {
 private:
     vector<int> *adjList;
     vector<bool> visited;
-    int numOfNodes;
+    int N; // nodes in graph;
 
 public:
-    Graph(int nodes) : numOfNodes(nodes)
+    Graph(int nodes) : N(nodes)
     {
         adjList = new vector<int> [nodes];
         visited.resize(nodes, 0);
     }
-    ~Graph(void) { delete[] adjList; }
-    void dfs(int start);
-    int connectedComp(void);
+    ~Graph(void) { delete[] adjList; visited.clear();}
     void addNode(int u, int v)
     {
         adjList[u].push_back(v);
-        adjList[v].push_back(u);
+        if (u != v) adjList[v].push_back(u);
     }
-};
-
-void Graph::dfs(int start)
-{
-    stack<int> stk;
-    stk.push(start);
-    visited[v] = 1;
-    while (!stk.empty()) {
-        int u = stk.top();
-        stk.pop();
-        for (auto &v : adjList[u]) {
-            if (visited[v]) continue;
-            visited[v] = 1;
-            stk.push(v);
-        }
-    }
-}
-
-int Graph::connectedComp(void)
-{
-    int cc = 0;
-    for (int i = 0; i < numOfNodes; i++) {
-        for (auto &u :  adjList[i]) {
-            if (!visited[u]) {
-                dfs(u);
-                ++cc;
+    void dfs(int start)
+    {
+        stack<int> stk;
+        stk.push(start), visited[start] = 1;
+        while (!stk.empty()) {
+            int u = stk.top(); stk.pop();
+            for (auto &v : adjList[u]) {
+                if (visited[v]) continue;
+                visited[v] = 1, stk.push(v);
             }
         }
     }
-    return cc;
-}
+    int connectedComp(void)
+    {
+        int cc = 0;
+        for (int i = 0; i < N; i++)
+            for (auto &u :  adjList[i])
+                if (!visited[u]) dfs(u), ++cc;
+        return cc;
+    }
+};
 
-int
-main(void)
+int main(void)
 {
     Graph g(10);
     g.addNode(1, 3);
