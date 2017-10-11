7adb7e293f587f3a2b01bd62dcaa37d411a2b8fb
cpuNram <rajeshamg@yahoo.com>
2017-10-10 21:47:16 -0500

    Create connCompWithDFS.cpp
    Task: Given an undirected graph with 𝑛 vertices and 𝑚 edges, compute the number of connected
              components in it.
    
    Input Format:  A graph is given in the standard format.
    
    Constraints: 1 ≤ 𝑛 ≤ 103, 0 ≤ 𝑚 ≤ 103.
    
    Output Format:  Output the number of connected components.
    
    Sample Test:
    4 2
    1 2
    3 2
    
    Expected Result: 2

---
+++ graph-theory/connCompWithDFS.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Graph {
+    private:
+        vector<int> *adjList;
+        vector<int> visited;
+        int N; // num of nodes in graphs.
+    public:
+        Graph(int nodes) : N(nodes)
+        {
+            adjList = new vector<int> [nodes];
+            visited.resize(nodes, 0);
+        }
+        ~Graph(void)
+        {
+            delete[] adjList; visited.clear();
+        }
+        void addNode(const int &u, const int &v)
+        {
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+            
+        }
+        void dfs(const int &u)
+        {
+            if (visited[u]) return;
+            visited[u] = 1;
+            for (auto &v : adjList[u]) dfs(v);
+        }
+        int connComp(void)
+        {
+            int cc = 0;
+            for (int i = 0; i < N; i++) {
+                for (auto &u : adjList[i]) {
+                    if (!visited[u]) {
+                        dfs(u), ++cc;
+                    }
+                }
+            }
+            return cc;
+        }
+};
+
+int main(void)
+{
+    int n, m;
+    cin >> n >> m; // n is num of nodes and m is num of edges.
+    Graph g(n);
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+        --u; --v;
+        g.addNode(u, v);
+    }
+    cout << g.connComp() << "\n";
+}
