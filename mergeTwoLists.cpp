a4037e6b62888f1663d4d19ac431ca5a3ccb1e49
cpuNram <rajeshamg@yahoo.com>
2017-11-06 20:50:36 -0600

    Update mergeTwoLists.cpp

--- mergeTwoLists.cpp
+++ mergeTwoLists.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 
 using namespace std;
-struct node {
+typedef struct node {
     int data;
     node *next;
-};
+} Node;
 
-typedef struct node Node;
-
-Node* addItem(Node *head, int item)
+void addItem(Node **head, int item)
 {
-    Node *tmpnode = nullptr;
-    tmpnode = new Node;
+    Node *tmpnode = new Node;
     tmpnode->data = item;
     tmpnode->next = nullptr;
-    if (!head) head = tmpnode;
+    if (!(*head)) *head = tmpnode;
     else {
-        Node *tmpnode1 = head;
-        while(tmpnode1->next) tmpnode1 = tmpnode1->next;
-        tmpnode1->next = tmpnode;
+        tmpnode->next = *head;
+        *head = tmpnode;
     }
-    return head;
+}
+
+void MoveData(Node **src, Node **dest)
+{
+    Node *tmpnode = *src;
+    *src = (*src)->next;
+    tmpnode->next = *dest;
+    *dest = tmpnode;
 }
 
 Node* mergeLists(Node *L1, Node *L2)
@@ -29,56 +32,52 @@ Node* mergeLists(Node *L1, Node *L2)
     if (!L1 && !L2) return nullptr;
     if (!L1) return L2;
     if (!L2) return L1;
-    Node *mergeList = nullptr;
+    Node mergeList;
+    mergeList.next = nullptr;
+    Node *tail = &mergeList;
 
     while (L1 && L2) {
-        int item = 0;
         if (L1->data <= L2->data) {
-            item = L1->data;
-            L1 = L1->next;
+            MoveData(&L1, &(tail->next));
         } else {
-            item = L2->data;
-            L2 = L2->next;
+            MoveData(&L2, &(tail->next));
         }
-        mergeList = addItem(mergeList, item);
+        tail = tail->next;
     }
 
-    while (L1) {
-        mergeList = addItem(mergeList, L1->data);
-        L1 = L1->next;
+    if (L1) {
+        tail->next = L1;
     }
-    while (L2) {
-        mergeList = addItem(mergeList, L2->data);
-        L2 = L2->next;
+    if (L2) {
+        tail->next = L2;
     }
 
-    return mergeList;
+    return mergeList.next;
 }
 
-Node* printList(Node* mergeList)
+void printList(Node* mergeList)
 {
     Node *head = mergeList;
     while (head) {
         cout << head->data << ", ";
         head = head->next;
     }
-    return mergeList;
+    cout << "\n";
 }
 
 int main(void)
 {
     Node *L1 = nullptr;
+    addItem(&L1, 4);
+    addItem(&L1, 3);
+    addItem(&L1, 2);
+    addItem(&L1, 1);
+    cout << "L1 : "; printList(L1);
     Node *L2 = nullptr;
-    L1 = addItem(L1, 1);
-    L1 = addItem(L1, 2);
-    L1 = addItem(L1, 3);
-    L1 = addItem(L1, 4);
-
-    L2 = addItem(L2, 3);
-    L2 = addItem(L2, 3);
-    L2 = addItem(L2, 5);
-    L2 = addItem(L2, 6);
-
-   Node* mergeList = mergeLists(L1, L2);
-   printList(mergeList);
+    addItem(&L2, 6);
+    addItem(&L2, 5);
+    addItem(&L2, 3);
+    cout << "L2 : "; printList(L2);
+    Node* mergeList = mergeLists(L1, L2);
+    cout << "Merged List: "; printList(mergeList);
 }
