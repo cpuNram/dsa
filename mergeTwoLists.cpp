f7a25e7245098a70ae41bfd9ee5a62ca9b30815b
cpuNram <rajeshamg@yahoo.com>
2017-11-06 21:34:45 -0600

    Update mergeTwoLists.cpp

--- mergeTwoLists.cpp
+++ mergeTwoLists.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-
 using namespace std;
+
 typedef struct node {
     int data;
     node *next;
 } Node;
+
+Node* mergeLists(Node* a, Node* b) 
+{
+    Node* result = NULL;
+ 
+    if (a == NULL) 
+        return b;
+    else if (b == NULL) 
+        return a;
+ 
+    if (a->data <= b->data) {
+        result = a;
+        result->next = mergeLists(a->next, b);
+    } else {
+        result = b;
+        result->next = mergeLists(a, b->next);
+    }
+    return(result);
+}
 
 void addItem(Node **head, int item)
 {
@@ -24,35 +43,6 @@ void MoveData(Node **src, Node **dest)
     *src = (*src)->next;
     tmpnode->next = *dest;
     *dest = tmpnode;
-}
-
-Node* mergeLists(Node *L1, Node *L2)
-{
-
-    if (!L1 && !L2) return nullptr;
-    if (!L1) return L2;
-    if (!L2) return L1;
-    Node mergeList;
-    mergeList.next = nullptr;
-    Node *tail = &mergeList;
-
-    while (L1 && L2) {
-        if (L1->data <= L2->data) {
-            MoveData(&L1, &(tail->next));
-        } else {
-            MoveData(&L2, &(tail->next));
-        }
-        tail = tail->next;
-    }
-
-    if (L1) {
-        tail->next = L1;
-    }
-    if (L2) {
-        tail->next = L2;
-    }
-
-    return mergeList.next;
 }
 
 void printList(Node* mergeList)
